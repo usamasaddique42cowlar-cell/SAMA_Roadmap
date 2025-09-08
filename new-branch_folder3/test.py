f14cd141ea238a5de18a6cac63cc66b559163e99
Usama <usama.saddique42.cowlar@gmail.com>
2025-09-07 17:01:21 -0700

    changes are added

---
+++ new-branch_folder3/test.py
@@ -0,0 +1,9 @@
+def average_salary(person1_salary,person2_salary):
+    person1_salar = person1_salary
+    person2_salar = person2_salary
+    average =  (person1_salar + person2_salar)/2
+    print(f"Average salary of the two persons is {average}")
+
+sal1 = 50
+sal2 = 60
+average_salary(sal1, sal2)
